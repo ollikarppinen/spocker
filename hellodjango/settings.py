134bc37875f06deddb1cf7febc8ecddfa804f524
ollikarppinen <ollikarppinen@gmail.com>
2015-12-28 14:46:17 +0200

    Registering fixed

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -143,6 +143,7 @@ ALLOWED_HOSTS = ['*']
 # Django registration redux settings
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+SITE_ID = 1
 
 # Crispy forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
