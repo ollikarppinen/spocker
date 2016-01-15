fbf800585d6a7de3ddf7d7b59a699d62edd4737a
ollikarppinen <ollikarppinen@gmail.com>
2016-01-15 18:47:54 +0200

    Added loading spinner to home

--- static/js/masonry.js
+++ static/js/masonry.js
@@ -1,8 +1,11 @@
 $(function() {
+  $('.image-container').hide();
   imagesLoaded('.image-container', function() {
+    $('.image-container').show();
     var $container = $('.image-container').masonry({
       itemSelector: '.grid-item',
       columnWidth:'.grid-item',
     });
+    $('.fa-spinner').hide();
   });
 });
